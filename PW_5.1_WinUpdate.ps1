8079fa5d9e4fe7c2176fceddbe244fb3f8d753b3
Alexandre DRUT <alexandre.drut@gmail.com>
2023-05-17 14:15:13 +0200

    update program

--- PW_5.1_WinUpdate.ps1
+++ PW_5.1_WinUpdate.ps1
@@ -1,28 +1,28 @@
 ﻿$dossier = "C:\users\manipulator\Desktop\Script_PS\PWS_v5"
 $start = Get-Date
-Write-Warning ("--- Start time of program : " + $start.ToLongTimeString())
+Write-Host [((Get-Date).ToLongTimeString())] "Start time of program " 
 
 #Execution de la fonction TestConnection
 
-Write-Warning "Test internet connection is running" 
+Write-Host [((Get-Date).ToLongTimeString())] "Test internet connection is running" 
 $TestConnection = powershell -command "& {. $dossier\TestConnection.ps1;TestConnection }"  
 
 #Message de l'état de connection Internet
-Write-Warning @{$True="Connection internet ok";$False="Internet connection failed - Update no executed "}[$TestConnection -eq "TRUE"]
+Write-Host [((Get-Date).ToLongTimeString())] @{$True="Connection internet ok";$False="Internet connection failed - Update no executed "}[$TestConnection -eq "TRUE"]
 
 switch($TestConnection)
 {
 #Connection réussi
     $True {
        #importation de la fonction de mise à jour
-       powershell -command "& {. $dossier\Update.ps1;UpdateData }"
+      powershell -command "& {. $dossier\Update.ps1;UpdateData }"
     }
 #Connection erreur
     $False {
-    Write-Error "Internet connection failed - No update founded"
+    Write-Host [((Get-Date).ToLongTimeString())] "Internet connection failed - No update founded"
     }
 }
 $end = Get-Date
-Write-Warning ("--- End time of program : " + $end.ToLongTimeString())
+Write-Host [((Get-Date).ToLongTimeString())] "End time of program"
 $total = $end - $start
-Write-Warning ($total.Hours.ToString()+" HH "+$total.Minutes.ToString()+" MM "+$total.Seconds.ToString()+" SS")
+Write-Host ($total.Hours.ToString()+" HH "+$total.Minutes.ToString()+" MM "+$total.Seconds.ToString()+" SS")
--- Update.ps1
+++ Update.ps1
@@ -1,54 +1,48 @@
 ﻿function UpdateData
 {   
     #Recherche de mise à jour dans le catalogue windows update
-    Write-Warning ("Research update..." + (Get-Date).ToLongTimeString())
+    Write-Host  [(Get-Date).ToLongTimeString()] "Research update"
     $update = Get-WUList
     #Totaliser le nombre mise à jour à faire
     $nbUpdateToDo = $update.count
-    Write-Warning ("Number of updates found : " + $nbUpdateToDo)
-    Write-Warning (Get-Date).ToLongTimeString()
+    Write-Host [(Get-Date).ToLongTimeString()] "Number of updates found : " + $nbUpdateToDo
+    
     
     if($nbUpdateToDo -gt 0 ) 
     { 
     #Début de l'installation
-        Write-Warning "...Starting installation..."
+        Write-Host [(Get-Date).ToLongTimeString()] "Starting installation"
 	    foreach($items in $update)
 	    {
 			try
 			{
-    
-                Write-Warning (Get-Date).ToLongTimeString()
-				Write-Warning ("---Starting update : " + $items.KB)
+				Write-Host [(Get-Date).ToLongTimeString()] "Starting update : " + $items.KB
 				Install-WindowsUpdate -AcceptAll -install
-				Write-Warning ("---Update completed : " + $items.KB)
-                Write-Warning (Get-Date).ToLongTimeString()
+				Write-Host [(Get-Date).ToLongTimeString()] "Update completed : " + $items.KB
 			}
 			catch
 			{
-                Write-Warning (Get-Date).ToLongTimeString()
+                Write-Warning [(Get-Date).ToLongTimeString()] "Error : "
 				Write-Error $_.Exception.Message
 			}
 	    }
 #Message de fin de mise à jour
-        write-Warning @{$true="--- Update is finished ---";$false="--- Updates are finished ---"}[$nbUpdateToDo -eq 1]
-        Write-Warning (Get-Date).ToLongTimeString()
+        write-Host [(Get-Date).ToLongTimeString()] @{$true="Update is finished";$false="Updates are finished"}[$nbUpdateToDo -eq 1]
 
 #Vérifier si le pc doit redémarre        
         $status = Get-WURebootStatus
         if($status.RebootRequired -eq 1)
         {
-            Write-Warning "Redémarrage en cours"
+            Write-Host "Redémarrage en cours"
             shutdown.exe /r /t 60 
         }
         else 
         { 
-            Write-Warning "---Not necessary to restart computer ---";
-            Write-Warning (Get-Date).ToLongTimeString()
+            Write-Host [(Get-Date).ToLongTimeString()] "Not necessary to restart computer ---";
         }   
     }
 else
 { 
-	Write-Warning "--- No update found , Pc is up to date ---" 
-    Write-Warning (Get-Date).ToLongTimeString()
+	Write-Host [(Get-Date).ToLongTimeString()] "No update found , Pc is up to date ---" 
 }
 }
